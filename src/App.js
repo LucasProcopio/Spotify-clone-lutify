d0572f0a3963fa32b89b1efbbf160c0a5c2c6758
Lucas Procopio <procmode.dev@gmailcom>
2019-07-29 22:02:22 -0300

    Styles: Reset css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
-    <div>
-      it works
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <div>it works</div>
+    </React.Fragment>
   );
 }
 
